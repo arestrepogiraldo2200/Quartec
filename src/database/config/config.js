748f07ef1ccd0a98bec157c692b42cef5163243f
AlejoRestrepo <alejandro.restrepo19@udea.edu.co>
2023-07-16 17:15:12 -0500

    local db

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -3,29 +3,29 @@ require('dotenv').config();
 
 // Local database ------------------------------------------
 
-// module.exports = {
-//   "development": {
-//     "username": process.env.USERNAMELOCAL,
-//     "password": process.env.PASSWORDLOCAL,
-//     "database": process.env.DATABASELOCAL,
-//     "host": process.env.HOSTLOCAL,
-//     "dialect": "mysql"
-//   },
-//   "test": {
-//     "username": process.env.USERNAMELOCAL,
-//     "password": process.env.PASSWORDLOCAL,
-//     "database": process.env.DATABASELOCAL,
-//     "host": process.env.HOSTLOCAL,
-//     "dialect": "mysql"
-//   },
-//   "production": {
-//     "username": process.env.USERNAMELOCAL,
-//     "password": process.env.PASSWORDLOCAL,
-//     "database": process.env.DATABASELOCAL,
-//     "host": process.env.HOSTLOCAL,
-//     "dialect": "mysql"
-//   }
-// }
+module.exports = {
+  "development": {
+    "username": process.env.USERNAMELOCAL,
+    "password": process.env.PASSWORDLOCAL,
+    "database": process.env.DATABASELOCAL,
+    "host": process.env.HOSTLOCAL,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": process.env.USERNAMELOCAL,
+    "password": process.env.PASSWORDLOCAL,
+    "database": process.env.DATABASELOCAL,
+    "host": process.env.HOSTLOCAL,
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": process.env.USERNAMELOCAL,
+    "password": process.env.PASSWORDLOCAL,
+    "database": process.env.DATABASELOCAL,
+    "host": process.env.HOSTLOCAL,
+    "dialect": "mysql"
+  }
+}
 
 // Clever cloud database ------------------------------------------
 
@@ -55,26 +55,26 @@ require('dotenv').config();
 
 // Familyapps Google cloud database ------------------------------------------
 
-module.exports = {
-  "development": {
-    "username": process.env.USERNAMEGOOGLE,
-    "password": process.env.PASSWORDGOOGLE,
-    "database": process.env.DATABASEGOOGLE,
-    "host": process.env.HOSTGOOGLE,
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": process.env.USERNAMEGOOGLE,
-    "password": process.env.PASSWORDGOOGLE,
-    "database": process.env.DATABASEGOOGLE,
-    "host": process.env.HOSTGOOGLE,
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": process.env.USERNAMEGOOGLE,
-    "password": process.env.PASSWORDGOOGLE,
-    "database": process.env.DATABASEGOOGLE,
-    "host": process.env.HOSTGOOGLE,
-    "dialect": "mysql"
-  }
-}
+// module.exports = {
+//   "development": {
+//     "username": process.env.USERNAMEGOOGLE,
+//     "password": process.env.PASSWORDGOOGLE,
+//     "database": process.env.DATABASEGOOGLE,
+//     "host": process.env.HOSTGOOGLE,
+//     "dialect": "mysql"
+//   },
+//   "test": {
+//     "username": process.env.USERNAMEGOOGLE,
+//     "password": process.env.PASSWORDGOOGLE,
+//     "database": process.env.DATABASEGOOGLE,
+//     "host": process.env.HOSTGOOGLE,
+//     "dialect": "mysql"
+//   },
+//   "production": {
+//     "username": process.env.USERNAMEGOOGLE,
+//     "password": process.env.PASSWORDGOOGLE,
+//     "database": process.env.DATABASEGOOGLE,
+//     "host": process.env.HOSTGOOGLE,
+//     "dialect": "mysql"
+//   }
+// }

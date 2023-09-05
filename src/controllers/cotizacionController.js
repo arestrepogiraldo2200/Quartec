33e9818af73a7712a18e28bead6a75aa64ee18d2
arestrepogiraldo2200 <arestrepogiraldo@gmail.com>
2023-09-05 17:26:19 -0500

    ttimmee

--- src/controllers/cotizacionController.js
+++ src/controllers/cotizacionController.js
@@ -443,7 +443,7 @@ let cotizacionController = {
         // Función para convertir a .pdf -------------------------------------------------------
         async function convertPDF(filenametoconvert) {
 
-            await wait(1000);
+            await wait(6000);
 
             const ext = 'pdf'; // Output extension
             const inputPath =  path.join("./public", filenametoconvert);
@@ -462,7 +462,7 @@ let cotizacionController = {
         // Función para borrar archivos -------------------------------------------------------
         async function deleteDirFilesUsingPattern(pattern){
 
-            await wait(6000);
+            await wait(8000);
 
             dirPath = path.join(__dirname, "../../public");
           
